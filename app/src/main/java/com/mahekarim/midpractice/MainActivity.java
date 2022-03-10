501cbf5808b073672cf5aef4673b133771870237
Mahe Karim <MaheKarim@deligram.com>
2022-03-11 03:03:52 +0600

    Relative , Constraint layout

--- app/src/main/java/com/mahekarim/midpractice/MainActivity.java
+++ app/src/main/java/com/mahekarim/midpractice/MainActivity.java
@@ -3,17 +3,26 @@ package com.mahekarim.midpractice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit1, edit2 ;
+    TextView textResult ;
+    Button btn1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Hiding Title And Title Bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_main);
+
+//        edit1 = (EditText)findViewById(R.id.edit1);
+//        edit2 = (EditText)findViewById(R.id.edit2);
+//        textResult = (TextView)findViewById(R.id.textResult);
+//        btn1 = (Button)findViewById(R.id.btn1);
+
 
     }
 }
