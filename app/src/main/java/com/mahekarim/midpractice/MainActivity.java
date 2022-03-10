8edf6c84b1a3a363063b87b4287f4671e2cf80be
Mahe Karim <MaheKarim@deligram.com>
2022-03-11 02:11:58 +0600

    Hiding Title And Title Bar

--- app/src/main/java/com/mahekarim/midpractice/MainActivity.java
+++ app/src/main/java/com/mahekarim/midpractice/MainActivity.java
@@ -2,12 +2,18 @@ package com.mahekarim.midpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Hiding Title And Title Bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
     }
 }
